a50a6dec0763dbd78339d14dd4ba0ba3833bbe20
Daniele Morosinotto <daniele.morosinotto@gmail.com>
2019-11-14 05:12:59 +0000

    3.3 VSO> ng update @angular/core --from 7 --to 8 --migrate-only

--- src/app/ng-version/ng-version.component.ts
+++ src/app/ng-version/ng-version.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, Renderer, ViewChild, ElementRef } from '@angular/core';
+import { Component, Input, OnInit, ViewChild, ElementRef, Renderer2 } from '@angular/core';
 declare var require: any;
 @Component({
   selector: 'app-ng-version',
@@ -10,12 +10,32 @@ declare var require: any;
 })
 export class NgVersionComponent implements OnInit {
   ngver: string;
-  constructor(private renderer: Renderer) { 
+  constructor(private renderer: Renderer2) { 
     this.ngver = require("@angular/core/package.json").version;
   }
   
-  @ViewChild('m') marquee: ElementRef;
+  @ViewChild('m', { static: true }) marquee: ElementRef;
   ngOnInit(): void {
-    this.renderer.setElementAttribute(this.marquee.nativeElement, "scrollamount", "10");
+    __ngRendererSetElementAttributeHelper(this.renderer, this.marquee.nativeElement, "scrollamount", "10");
   }
+}
+
+type AnyDuringRendererMigration = any;
+
+function __ngRendererSplitNamespaceHelper(name: AnyDuringRendererMigration) {
+    if (name[0] === ":") {
+        const match = name.match(/^:([^:]+):(.+)$/);
+        return [match[1], match[2]];
+    }
+    return ["", name];
+}
+
+function __ngRendererSetElementAttributeHelper(renderer: AnyDuringRendererMigration, element: AnyDuringRendererMigration, namespaceAndName: AnyDuringRendererMigration, value?: AnyDuringRendererMigration) {
+    const [namespace, name] = __ngRendererSplitNamespaceHelper(namespaceAndName);
+    if (value != null) {
+        renderer.setAttribute(element, name, value, namespace);
+    }
+    else {
+        renderer.removeAttribute(element, name, namespace);
+    }
 }
